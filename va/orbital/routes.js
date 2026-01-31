b20aab7f5af395f9fbcf805547f77998853cff32
HugoCNRD <hugobatigne@gmail.com>
2026-01-31 15:47:09 +0100

    Update routes.js

--- va/orbital/routes.js
+++ va/orbital/routes.js
@@ -1,1 +1,34 @@
+/* =====================================
+   ORBITAL – ROUTES DATABASE (FRONT ONLY)
+===================================== */
+
+const routes = [
+  {
+    dep: "LFPG",
+    arr: "KJFK",
+    duration: "07h30",
+    grade: "FO",
+    aircraft: "A350",
+    livery: "Orbital",
+    type: "Long Haul"
+  },
+  {
+    dep: "EGLL",
+    arr: "EDDF",
+    duration: "01h25",
+    grade: "FO",
+    aircraft: "A320",
+    livery: "Orbital",
+    type: "Short Haul"
+  },
+  {
+    dep: "KSFO",
+    arr: "PHNL",
+    duration: "05h45",
+    grade: "CPT",
+    aircraft: "B787",
+    livery: "Orbital",
+    type: "Medium Haul"
+  }
+];
 
