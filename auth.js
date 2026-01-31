8209b574efd69247fa76ff9f6e7f9cbbbce27556
HugoCNRD <hugobatigne@gmail.com>
2026-01-31 15:41:25 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,29 @@
+import { auth, db } from "./firebase.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { doc, setDoc } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+window.login = () => {
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then(() => location.href = "home.html");
+};
+
+window.signup = async () => {
+  const user = await createUserWithEmailAndPassword(
+    auth,
+    signupEmail.value,
+    signupPassword.value
+  );
+
+  await setDoc(doc(db, "users", user.user.uid), {
+    ifc: ifc.value,
+    callsign: callsign.value,
+    va: va.value,
+    pireps: 0,
+    role: "pilot"
+  });
+
+  location.href = "home.html";
+};
 
